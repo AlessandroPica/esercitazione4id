f41d708536d8474c2e8f1e443c38d3eddf4ca89e
AlessandroPica <pica.alessandro.studente@itispaleocapa.it>
2024-09-20 09:40:09 +0200

    metodo main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package concessionario;
+import  java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in =new Scanner(System.in);
+        int scelta;
+        do{
+            System.out.println("CONCESSIONARIO AUTO");
+            System.out.println("Aggiungi auto - 1");
+            System.out.println("Visualizza le auto - 2");
+            System.out.println("Ricerca di un'auto - 3");
+            System.out.println("Cancella auto - 4");
+            System.out.println("Modifica dati auto - 5");
+            System.out.println("Visualizza auto in ordine crescente (di prezzo) - 6");
+            System.out.println("Visualizza le auto che utilizzano il diesel - 7");
+            System.out.println("Chiudi il menù - 8");
+            scelta = in.nextInt();
+            switch (scelta){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+            }
+        }while (scelta != 8);
     }
 }
---
+++ src/Metodi.java
@@ -0,0 +1,5 @@
+package concessionario;
+
+public class Metodi {
+
+}
